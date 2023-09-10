11ea142e40da04c482ab46e99a5ab2640fbb7079
Son-2003 <nguyenducson2915@gmail.com>
2023-09-10 18:33:44 +0700

    API Basic

---
+++ ReStoreBE/ReStore/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReStore.Data;
+using ReStore.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ReStore.Controllers
+{
+    [ApiController] //Anotation đánh dấu đây là apicontroller
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //api/products/3
+        public async Task<ActionResult<Product>> GetProducts(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
---
+++ ReStoreBE/ReStore/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using ReStore.Entities;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ReStore.Data
+{
+        public class StoreContext : DbContext
+        {
+            public StoreContext(DbContextOptions options) : base(options)
+            {
+            }
+            public DbSet<Product> Products { get; set; }
+        }
+    
+}
